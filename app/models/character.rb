f8a542ba7f5582d952bb9da93f9b371934959ac0
OM Miles <miles@ommiles.com>
2021-05-25 19:34:52 -0700

    lab complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,12 @@
 class Character < ActiveRecord::Base
     belongs_to :show
     belongs_to :actor
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
+
+    def actors_list
+        self.show.characters.actors.map{|actor| actor.name}
+    end
 end
